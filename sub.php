e93383a993e8aad26208915f73baac50cd388301
kimyh5392 <mk.kimyh@gmail.com>
2017-04-28 18:37:00 +0900

    서브 페이지 레이아웃 및 가이드 퍼블리싱 #1

---
+++ sub.php
@@ -0,0 +1,348 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>우정BSC</title>
+	<link href="/assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	<link href="/assets/fontello/css/fontello.css" rel="stylesheet">
+	<link href="/assets/css/default.css" rel="stylesheet">
+	<link href="/assets/css/sub.css" rel="stylesheet">
+	<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+</head>
+<body class="nav-opened">
+
+	<nav id="global-nav" class="visible-lg">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6">
+					<div class="affiliates">
+						<ul>
+							<li><a href="#">우정BSC</a></li>
+							<li><a href="#">우정E&amp;C</a></li>
+							<li><a href="#">INNOSERVICE</a></li>
+							<li><a href="#">BSMS</a></li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-md-6">
+					<div class="util">
+						<ul>
+							<li><a href="#">채용정보</a></li>
+							<li><a href="#">E-러닝</a></li>
+							<li><a href="#">E-카탈로그</a></li>
+						</ul>
+					</div>
+					<div class="sns">
+						<ul>
+							<li class="fb"><a href="#"><i class="icon-facebook"></i><span class="sr-only">페이스북</span></a></li>
+							<li class="bl"><a href="#"><i>b</i><span class="sr-only">블로그</span></a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</nav>
+	
+	<!-- header id="header">
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-4 col-xs-push-8 hidden-lg">
+					<button id="btn-nav-open" class="btn btn-link hidden-lg" type="button">
+						<span class="sr-only">메뉴 열기</span>
+					</button>
+				</div>
+				<div class="col-xs-8 col-xs-pull-4 col-lg-3 col-lg-pull-0">
+					<h1 class="logo"><a href="/"><img src="/assets/images/logo.png" alt="우정BSC"></a></h1>
+				</div>
+				<div class="col-lg-9 visible-lg">
+					<nav id="nav">
+						<ul>
+							<li>
+								<a href="#">회사소개<span></span></a>
+								<ul>
+									<li><a href="#">CEO인사말</a></li>
+									<li><a href="#">비전</a></li>
+									<li><a href="#">연혁</a></li>
+									<li><a href="#">CI소개</a></li>
+									<li><a href="#">조직도</a></li>
+									<li><a href="#">협력사</a></li>
+									<li><a href="#">오시는 길</a></li>
+								</ul>
+							</li>
+							<li>
+								<a href="#">사업영역<span></span></a>
+								<ul>
+									<li><a href="#">TEST</a></li>
+									<li><a href="#">TEST</a></li>
+									<li><a href="#">TEST</a></li>
+								</ul>
+							</li>
+							<li>
+								<a href="#">연구개발<span></span></a>
+								<ul>
+									<li><a href="#">TEST</a></li>
+									<li><a href="#">TEST</a></li>
+									<li><a href="#">TEST</a></li>
+								</ul>
+							</li>
+							<li>
+								<a href="#">투자정보<span></span></a>
+								<ul>
+									<li><a href="#">TEST</a></li>
+									<li><a href="#">TEST</a></li>
+									<li><a href="#">TEST</a></li>
+								</ul>
+							</li>
+							<li>
+								<a href="#">고객센터<span></span></a>
+								<ul>
+									<li><a href="#">TEST</a></li>
+									<li><a href="#">TEST</a></li>
+									<li><a href="#">TEST</a></li>
+								</ul>
+							</li>
+						</ul>
+					</nav>
+				</div>
+			</div>
+		</div>
+	</header>
+	
+	<!--aside id="nav-aside" class="hidden-lg">
+		<div class="nav-content">
+			<div class="nav-header">
+				<button type="button" id="btn-nav-close" class="btn btn-link">
+					<span class="sr-only">메뉴 닫기</span>
+					<i class="icon-cancel"></i>
+				</button>
+				<ul class="sns">
+					<li><a class="fb" href="#"><i class="icon-facebook"></i></a></li>
+					<li><a class="bl" href="#"><i>b</i></a></li>
+				</ul>
+			</div>
+			<div class="nav-body">
+				<ul>
+					<li>
+						<a href="#">회사소개</a>
+						<ul>
+							<li><a href="#">CEO인사말</a></li>
+							<li><a href="#">비전</a></li>
+							<li><a href="#">연혁</a></li>
+							<li><a href="#">CI소개</a></li>
+							<li><a href="#">조직도</a></li>
+							<li><a href="#">협력사</a></li>
+							<li><a href="#">오시는 길</a></li>
+						</ul>
+					</li>
+					<li>
+						<a href="#">사업영역</a>
+						<ul>
+							<li><a href="#">TEST</a></li>
+							<li><a href="#">TEST</a></li>
+							<li><a href="#">TEST</a></li>
+						</ul>
+					</li>
+					<li>
+						<a href="#">연구개발</a>
+						<ul>
+							<li><a href="#">TEST</a></li>
+							<li><a href="#">TEST</a></li>
+							<li><a href="#">TEST</a></li>
+						</ul>
+					</li>
+					<li>
+						<a href="#">투자정보</a>
+						<ul>
+							<li><a href="#">TEST</a></li>
+							<li><a href="#">TEST</a></li>
+							<li><a href="#">TEST</a></li>
+						</ul>
+					</li>
+					<li>
+						<a href="#">고객센터</a>
+						<ul>
+							<li><a href="#">TEST</a></li>
+							<li><a href="#">TEST</a></li>
+							<li><a href="#">TEST</a></li>
+						</ul>
+					</li>
+				</ul>
+			</div>
+			<div class="nav-footer">
+				<1?php include($_SERVER['DOCUMENT_ROOT']."/inc/family-sites.php"); ?>
+			</div>
+		</div>
+	</aside-->
+
+
+
+
+
+
+
+	<main id="content" class="sub">
+        <div class="page-header">
+            <div class="visual-header">
+                <div class="header-intro">
+                    <h2>COMPANY</h2>
+                    <p class="hidden-xs">
+                        국내 최고의 바이오 인프라 솔루션 기업 우정비에스씨<br>
+                        지금까지 걸어온 30년... 이제부터가 시작입니다.
+                    </p>
+                </div>
+            </div>
+            <div class="sub-lnb dropdown">
+                <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true" class="hidden-lg">CEO 인사말</button>
+                <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
+                    <li class="active"><a href="#">CEO인사말</a></li>
+                    <li><a href="#">비전</a></li>
+                    <li><a href="#">연혁</a></li>
+                    <li><a href="#">CI소개</a></li>
+                    <li><a href="#">조직도</a></li>
+                    <li><a href="#">협력사</a></li>
+                    <li><a href="#">오시는길</a></li>
+                </ul>
+            </div>
+        </div>
+
+        <section>
+           <header>
+               <h3>CEO 인사말</h3>
+               <h4>Protect Your<br class="visible-xs">Research Investment</h4>
+               <p class="summary">
+                   Since 1989, 생명과학연구 솔루션 전문기업으로 도약해온<br class="hidden-xs">
+                   우정BSC를 찾아주신 여러분 반갑습니다.
+               </p>
+           </header>
+        </section>
+
+        <div class="container">
+            <!-- 검색폼 -->
+            <div class="search-form">
+               <form action="#">
+                   <select>
+                       <option>전체</option>
+                       <option>선택1</option>
+                       <option>선택2</option>
+                       <option>선택3</option>
+                   </select>
+                   <div class="form-group">
+                        <input type="text" id="search" class="form-control">
+                        <label for="search" class="sr-only">검색하기</label>
+                   </div>
+                   <button type="submit" class="btn btn-search">SEARCH</button>
+               </form>
+            </div>
+            
+            <div style="margin: 30px 0;"></div>
+            
+            <!-- 페이징 -->
+            <nav class="paging" aria-label="Page navigation">
+                <ul class="pagination">
+                    <li class="stp">
+                        <a href="#" aria-label="Previous">
+                            <i class="icon-angle-double-left"></i>
+                        </a>
+                    </li>
+                    <li class="stp stm-right">
+                        <a href="#" aria-label="Previous">
+                            <i class="icon-angle-left"></i>
+                        </a>
+                    </li>
+                    <li class="active"><a href="#">1</a></li>
+                    <li><a href="#">2</a></li>
+                    <li><a href="#">3</a></li>
+                    <li><a href="#">4</a></li>
+                    <li><a href="#">5</a></li>
+                    <li class="hidden-xs"><a href="#">6</a></li>
+                    <li class="hidden-xs"><a href="#">7</a></li>
+                    <li class="hidden-xs"><a href="#">8</a></li>
+                    <li class="hidden-xs"><a href="#">9</a></li>
+                    <li class="hidden-xs"><a href="#">10</a></li>
+                    <li class="stp stm-left">
+                        <a href="#" aria-label="Next">
+                            <i class="icon-angle-right"></i>
+                        </a>
+                    </li>
+                    <li class="stp">
+                        <a href="#" aria-label="Next">
+                            <i class="icon-angle-double-right"></i>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+       </div>
+	</main>
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	<nav id="foot-nav">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-9">#9</div>
+				<div class="col-lg-3">#3</div>
+			</div>
+		</div>
+	</nav>
+	
+	<footer id="footer">
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-12 col-lg-9">
+					<div class="copyright">
+						<p>COPYRIGHT (C) 2017 WOOJUNGBSC.,CO.LTD.  ALL RIGHTS RESERVED. SITE BY <a href="#" target="_blank">MOVEMENT K GROUP</a>.</p>
+					</div>
+				</div>
+				<div class="col-lg-3 visible-lg">
+					관련사이트
+				</div>
+			</div>
+		</div>
+	</footer>
+	
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+	<script src="/assets/bootstrap/js/bootstrap.min.js"></script>
+	
+	<script>
+		(function($) {
+			
+			/* 네비게이션 보이기, 숨기기 관련 이벤트 */
+			$(document).on('click', '#header #btn-nav-open', function() {
+				$('body').addClass('nav-opened');
+			});
+			$(document).on('click', '#nav-aside', function(e) {
+				if ($(e.target).attr('id') == 'nav-aside') {
+					$('body').removeClass('nav-opened');
+				}
+			});
+			$(document).on('click', '#nav-aside #btn-nav-close', function() {
+				$('body').removeClass('nav-opened');
+			});
+			
+			/* GNB 관련 */
+			$(document).on('click', '#nav-aside .nav-body > ul > li > a', function(e) {
+				if ($(this).parent().hasClass('selected')) {
+					$(this).parent().removeClass('selected');
+				} else {
+					$(this).parent().siblings('.selected').removeClass('selected');
+					$(this).parent().addClass('selected');
+				}
+				e.preventDefault();
+			});
+			
+		})(jQuery);
+	</script>
+	
+</body>
+</html>
