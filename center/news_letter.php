031ae2fe2f3d239eadcde5bd3ab908847bb50ccb
kimyh5392 <mk.kimyh@gmail.com>
2017-05-01 18:31:01 +0900

    고객센터 > 뉴스레터, 공지사항(리스트, 뷰) 완료

---
+++ center/news_letter.php
@@ -0,0 +1,185 @@
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/dochead.php'); ?>
+<link href="/assets/css/sub.css" rel="stylesheet">
+<link href="/assets/css/center.css" rel="stylesheet">
+</head>
+<body class="sub">
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/top-nav.php'); ?>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/header.php'); ?>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/nav-aside.php'); ?>
+    <!-- Sub Content Start -->
+    <main id="content" class="center">
+        <!-- Page Header -->
+        <div class="page-header">
+            <div class="visual-header">
+                <div class="header-intro">
+                    <h2>CS CENTER</h2>
+                    <p class="hidden-xs">
+                        국내 최고의 바이오 인프라 솔루션 기업 우정비에스씨<br>
+                        지금까지 걸어온 30년... 이제부터가 시작입니다.
+                    </p>
+                </div>
+            </div>
+            <div class="sub-lnb dropdown">
+                <button class="btn btn-default dropdown-toggle hidden-lg" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                    뉴스레터
+                </button>
+                <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
+                    <li><a href="/center/notice_list.php">공지사항</a></li>
+                    <li class="active"><a href="#">뉴스레터</a></li>
+                    <li><a href="#">E-카달로그</a></li>
+                    <li><a href="#">고객문의</a></li>
+                </ul>
+            </div>
+        </div>
+        <!-- Sub Page 공통 -->
+        <section class="news-letter">
+            <header>
+                <h3>뉴스레터</h3>
+                <p class="summary">
+                    우정BSC의 새로운 소식 및 공지사항을<br class="visible-xs">
+                    알려드립니다.
+                </p>
+            </header>
+            <div class="section-content">
+                <div class="container">
+                    <div class="search-form">
+                        <form action="#">
+                            <select>
+                                <option>전체</option>
+                                <option>선택1</option>
+                                <option>선택2</option>
+                                <option>선택3</option>
+                            </select>
+                            <div class="form-group">
+                                <input type="text" id="search" class="form-control">
+                                <label for="search" class="sr-only">검색하기</label>
+                            </div>
+                            <button type="submit" class="btn btn-search">SEARCH</button>
+                        </form>
+                    </div>
+                    <ul class="news-list">
+                        <li>
+                            <a href="#">
+                                <p class="figure">
+                                    <img src="/assets/images/center/news_letter_img01.jpg" class="img-responsive" alt="">
+                                </p>
+                                <div class="details">
+                                    <h4>
+                                        결과분석에서 논문데이터까지 단 한번의 실험으로 해결하세요!
+                                    </h4>
+                                    <p class="summary">
+                                        결과분석에서 논문데이터까지 단 한번의 실험으로 해결하세요!결과분석에서 논문데이터까지 단 한번의 실험으로 해결하세요!결과분석에서 논문데이터까지 단 한번의 실험으로 해결하세요!
+                                    </p>
+                                    <p class="date"><i class="icon-calendar"></i>2017-04-04</p>
+                                </div>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <p class="figure">
+                                    <img src="/assets/images/center/news_letter_img02.jpg" class="img-responsive" alt="">
+                                </p>
+                                <div class="details">
+                                    <h4>
+                                        결과분석에서 논문데이터까지 단 한번의 실험으로 해결하세요!
+                                    </h4>
+                                    <p class="summary">
+                                        결과분석에서 논문데이터까지 단 한번의 실험으로 해결하세요!결과분석에서 논문데이터까지 단 한번의 실험으로 해결하세요!결과분석에서 논문데이터까지 단 한번의 실험으로 해결하세요!
+                                    </p>
+                                    <p class="date"><i class="icon-calendar"></i>2017-04-04</p>
+                                </div>
+                            </a>
+                        </li>
+                        <li class="hidden-xs">
+                            <a href="#">
+                                <p class="figure">
+                                    <img src="/assets/images/center/news_letter_none_img.jpg" class="img-responsive" alt="">
+                                </p>
+                                <div class="details">
+                                    <h4>
+                                        결과분석에서 논문데이터까지 단 한번의 실험으로 해결하세요!
+                                    </h4>
+                                    <p class="summary">
+                                        결과분석에서 논문데이터까지 단 한번의 실험으로 해결하세요!결과분석에서 논문데이터까지 단 한번의 실험으로 해결하세요!결과분석에서 논문데이터까지 단 한번의 실험으로 해결하세요!
+                                    </p>
+                                    <p class="date"><i class="icon-calendar"></i>2017-04-04</p>
+                                </div>
+                            </a>
+                        </li>
+                        <li class="hidden-xs">
+                            <a href="#">
+                                <p class="figure">
+                                    <img src="/assets/images/center/news_letter_img04.jpg" class="img-responsive" alt="">
+                                </p>
+                                <div class="details">
+                                    <h4>
+                                        결과분석에서 논문데이터까지 단 한번의 실험으로 해결하세요!
+                                    </h4>
+                                    <p class="summary">
+                                        결과분석에서 논문데이터까지 단 한번의 실험으로 해결하세요!결과분석에서 논문데이터까지 단 한번의 실험으로 해결하세요!결과분석에서 논문데이터까지 단 한번의 실험으로 해결하세요!
+                                    </p>
+                                    <p class="date"><i class="icon-calendar"></i>2017-04-04</p>
+                                </div>
+                            </a>
+                        </li>
+                        <li class="hidden-xs">
+                            <a href="#">
+                                <p class="figure">
+                                    <img src="/assets/images/center/news_letter_none_img.jpg" class="img-responsive" alt="">
+                                </p>
+                                <div class="details">
+                                    <h4>
+                                        결과분석에서 논문데이터까지 단 한번의 실험으로 해결하세요!
+                                    </h4>
+                                    <p class="summary">
+                                        결과분석에서 논문데이터까지 단 한번의 실험으로 해결하세요!결과분석에서 논문데이터까지 단 한번의 실험으로 해결하세요!결과분석에서 논문데이터까지 단 한번의 실험으로 해결하세요!
+                                    </p>
+                                    <p class="date"><i class="icon-calendar"></i>2017-04-04</p>
+                                </div>
+                            </a>
+                        </li>
+                    </ul>
+                    <nav class="paging" aria-label="Page navigation">
+                        <ul class="pagination">
+                            <li class="stp">
+                                <a href="#" aria-label="Previous">
+                                    <i class="icon-angle-double-left"></i>
+                                </a>
+                            </li>
+                            <li class="stp stm-right">
+                                <a href="#" aria-label="Previous">
+                                    <i class="icon-angle-left"></i>
+                                </a>
+                            </li>
+                            <li class="active"><a href="#">1</a></li>
+                            <li><a href="#">2</a></li>
+                            <li><a href="#">3</a></li>
+                            <li><a href="#">4</a></li>
+                            <li><a href="#">5</a></li>
+                            <li class="hidden-xs"><a href="#">6</a></li>
+                            <li class="hidden-xs"><a href="#">7</a></li>
+                            <li class="hidden-xs"><a href="#">8</a></li>
+                            <li class="hidden-xs"><a href="#">9</a></li>
+                            <li class="hidden-xs"><a href="#">10</a></li>
+                            <li class="stp stm-left">
+                                <a href="#" aria-label="Next">
+                                    <i class="icon-angle-right"></i>
+                                </a>
+                            </li>
+                            <li class="stp">
+                                <a href="#" aria-label="Next">
+                                    <i class="icon-angle-double-right"></i>
+                                </a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </section>
+	</main>
+    <!-- //Sub Content Start -->
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/foot-nav.php'); ?>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/footer.php'); ?>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/docfoot.php'); ?>
+</body>
+</html>
---
+++ center/notice_view.php
@@ -0,0 +1,103 @@
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/dochead.php'); ?>
+<link href="/assets/css/sub.css" rel="stylesheet">
+<link href="/assets/css/center.css" rel="stylesheet">
+</head>
+<body class="sub">
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/top-nav.php'); ?>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/header.php'); ?>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/nav-aside.php'); ?>
+    <!-- Sub Content Start -->
+    <main id="content" class="center">
+        <!-- Page Header -->
+        <div class="page-header">
+            <div class="visual-header">
+                <div class="header-intro">
+                    <h2>CS CENTER</h2>
+                    <p class="hidden-xs">
+                        국내 최고의 바이오 인프라 솔루션 기업 우정비에스씨<br>
+                        지금까지 걸어온 30년... 이제부터가 시작입니다.
+                    </p>
+                </div>
+            </div>
+            <div class="sub-lnb dropdown">
+                <button class="btn btn-default dropdown-toggle hidden-lg" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                    공지사항
+                </button>
+                <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
+                    <li class="active"><a href="/center/notice_list.php">공지사항</a></li>
+                    <li><a href="#">뉴스레터</a></li>
+                    <li><a href="#">E-카달로그</a></li>
+                    <li><a href="#">고객문의</a></li>
+                </ul>
+            </div>
+        </div>
+        <!-- Sub Page 공통 -->
+        <section class="notice-view">
+            <header>
+                <h3>공지사항</h3>
+                <p class="summary">
+                    우정BSC의 새로운 소식 및 공지사항을<br class="visible-xs">
+                    알려드립니다.
+                </p>
+            </header>
+            <div class="section-content">
+                <div class="container">
+                    <article class="view">
+                        <div class="article-header">
+                            <h4>공고합병 및 주권제출공고</h4>
+                            <ul>
+                                <li>2017-04-04</li>
+                                <li>
+                                    <dl>
+                                        <dt>조회수</dt>
+                                        <dd>356</dd>
+                                    </dl>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="view-content">
+                            <p class="view-txt">
+                                한화엠지아이기업인수목적 주식회사(갑)는 2017년 3월 7일 개최한 임시 주주총회에서<br><br>
+                                주식회사 우정비에스씨(을)은 2017년 3월 7일 개최한 임시 주주총회에서 각각 합병을 결의하고 그 결과로<br><br>
+                                (갑)은 (을)을 합병하여 그 권리의무를 승계하고, (갑)은 존속하며 (을)은 해산하기로 결의하였으므로,<br><br>
+                                이 합병에 대하여 이의가 있는 채권자는 이 공고일 익일부터 1개월 이내에 관계회사에<br><br>
+                                이의를 제출하시고 주권을 가지고 있는 분은 관계회사에 주권을 제출하여 주시기 바라와 이에 공고 합니다.
+                            </p>
+                            <p class="figure">
+                                <img src="/assets/images/center/view_img_txt.jpg" class="img-responsive" alt="">
+                            </p>
+                        </div>
+                        <ul class="next-link">
+                            <li>
+                                <dl>
+                                    <dt>다음글</dt>
+                                    <dd>
+                                        <a href="#">다음글이 없습니다.</a>
+                                    </dd>
+                                </dl>
+                            </li>
+                            <li>
+                                <dl>
+                                    <dt>이전글</dt>
+                                    <dd>
+                                        <a href="#">[2017.02.17] 합병승인 임시주주총회 소집통지 공고</a>
+                                    </dd>
+                                </dl>
+                            </li>
+                        </ul>
+                    </article>
+                    <div class="btn-area">
+                        <p>
+                            <a href="/center/notice_list.php" class="btn btn-orange" role="button">목록으로</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+	</main>
+    <!-- //Sub Content Start -->
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/foot-nav.php'); ?>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/footer.php'); ?>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/docfoot.php'); ?>
+</body>
+</html>
